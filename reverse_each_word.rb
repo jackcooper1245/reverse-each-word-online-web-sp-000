bf3218e1f461bbba1814106878cd5af5a65b51ae
Jack Cooper <jackcooper1245@gmail.com>
2020-04-07 16:24:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(sentence)
 end
 return_sentence.join(" ")
 end
+
+def reverse_each_word(sentence)
+ new_sentence = sentence.split(" ")
+ return_sentence = []
+ new_sentence.collect do |string|
+   return_sentence << string.reverse
+end
+end
